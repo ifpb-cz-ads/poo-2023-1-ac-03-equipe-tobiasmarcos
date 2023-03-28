ed8924ca54cc5e8be57234ad3f76497937c345c9
Tobias-Santos <117104950+tobias-santos@users.noreply.github.com>
2023-03-28 18:03:53 -0300

    Adicionando 3 questões.

---
+++ CBBAtividadePratica2/CBBAtividadePratica2.java
@@ -0,0 +1,16 @@
+
+
+public class CBBAtividadePratica2
+{
+   public static void main(String[] args){
+       int var1 = 2;
+       int var2 = 4;
+       int var3 = 6;
+        int soma = var1 + var2 + var3;
+        int media = soma/3;
+        
+        System.out.println("A soma é: "+soma);
+        System.out.println("A média é: "+media);
+   }
+    
+}
---
+++ CBBAtividadePratica3/CBBAtividadePratica3.java
@@ -0,0 +1,19 @@
+
+/**
+ * Escreva uma descrição da classe CBBAtividadePratica3 aqui.
+ * 
+ * @author (seu nome) 
+ * @version (um número da versão ou uma data)
+ */
+public class CBBAtividadePratica3
+{
+   public static void main(String[] args){
+      int lado = 5;
+      int altura = 3;
+      
+       int areaRetangulo = (lado*altura)/2;
+       System.out.println("O retângulo possui aproximadamente "+areaRetangulo+" unidade(s) de área!");
+    }
+   
+    
+}
