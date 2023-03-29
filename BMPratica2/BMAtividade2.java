17ecb865c70f08ab1695ead9e2f2b97deb0687e5
Marcos Paulo <103538721+KingZabitus@users.noreply.github.com>
2023-03-29 19:30:28 -0300

    Add files via upload

---
+++ BMPratica2/BMAtividade2.java
@@ -0,0 +1,12 @@
+public class BMAtividade2 {
+    public static void main(String[] args) {
+        int i = 1;
+        long j = 2L;
+        float p = 20.0f;
+        double q = 30.0;
+        boolean b = true;
+        char c = 'k';
+        
+        System.out.println("i = " + i + ", j = " + j + ", p = " + p + ", q = " + q + ", b = " + b + ", c = " + c);
+    }
+}
